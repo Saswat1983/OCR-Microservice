2ad7b65bf70ac38b620b944bce5b9190f8fa9055
Saswat1983 <113896968+Saswat1983@users.noreply.github.com>
2023-08-22 21:25:58 -0400

    Add files via upload

---
+++ OCR.Microservice/OCR.Service/Controllers/RecognizeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System.Web.Http;
+
+
+namespace OCR.Api.Service.Controllers
+{
+    public class RecognizeController : ApiController
+    {
+        // GET: RecognizeController
+
+        // POST: RecognizeController/Create
+
+
+        // GET: RecognizeController/Edit/5
+
+
+        // POST: RecognizeController/Edit/5
+
+
+
+        // POST: RecognizeController/Delete/5
+        [HttpPost]
+        public IHttpActionResult Check(int id, IFormCollection collection)
+        {
+            return Ok();
+        }
+    }
+}
